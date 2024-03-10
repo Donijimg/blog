0e8017793cd7f5cc2c7397708c4433e92b3c9622
Donijimg <doncanjimenez5@gmail.com>
2024-03-09 19:55:21 -0500

    blog

--- post/views.py
+++ post/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Autor
 # Create your views here.
 
 def index(request):
   posts = Post.objects.all()
+  autors = Autor.objects.all()
   for obj in posts:
     print(obj.titulo)
+  for obj in autors:
+    print(obj.nombre)
   return HttpResponse ("lista de posts")
 
-def storage(request, titulo, cuerpo):
+def storage(request, titulo, cuerpo, nombre,correo):
   post = Post(titulo=titulo, cuerpo=cuerpo)
+  autor = Autor(nombre=nombre, correo=correo)
   post.save()
+  autor.save()
   return HttpResponse ("guardamos los datos")
   
 def consultar(requets,id):
   post= Post.objects.get(id=id)
+  autor= Autor.objects.get(id=id)
   print (post)
+  print (autor)
   return HttpResponse (f"titulo: {post.titulo}, cuerpo:{post.cuerpo}, fecha: {post.fecha}")
 
 def modificar(request, titulo,id):
@@ -29,6 +36,3 @@ def eliminar(request,id):
   post=Post.objects.get(id=id)
   post.delete()
   return HttpResponse("post eliminado")
-
-
-
